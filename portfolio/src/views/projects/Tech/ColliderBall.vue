4ad09845f2521c8f1d5a6846c39228a0e9652214
miatang <miatang13@gmail.com>
2021-01-11 20:43:43 -0800

    collider gifs

--- portfolio/src/views/projects/Tech/ColliderBall.vue
+++ portfolio/src/views/projects/Tech/ColliderBall.vue
@@ -32,14 +32,19 @@
       <div class="column is-full"><hr /></div>
 
       <div class="column is-two-thirds">
-        <span class="section__title"> About </span>
+        <span class="section__title"> Overview </span>
         <p class="body__text">
           Creating animations through code gives enormous freedom, and I enjoyed
           creating animations that either represents real-world occurrences or
           allows perfect mechanical geometries that we will never see in the
-          world.
+          world. <br />
+          <br />
+          This is a selected creative coding project. Feel free to push the ball
+          in different directions. Full screen experience is recommended.
         </p>
       </div>
+
+      <div class="column is-full"><hr /></div>
 
       <div class="column is-full">
         <div class="glitch-embed-wrap" style="height: 800px; width: 100%;">
@@ -50,6 +55,52 @@
             style="height: 100%; width: 100%; border: 0;"
           >
           </iframe>
+        </div>
+      </div>
+
+      <div class="column is-full"><hr /></div>
+      <div class="column is-two-thirds">
+        <span class="section__title"> Interactions Demos </span>
+      </div>
+
+      <span class="image__caption ibm__typeface column is-full">
+        1. No interaction
+      </span>
+
+      <div class="columns is-centered">
+        <div class="column is-full">
+          <img
+            class="image"
+            src="../../../assets/img/ColliderBall/no_interaction.gif"
+          />
+        </div>
+      </div>
+      <div class="column is-full"><hr /></div>
+
+      <span class="image__caption ibm__typeface column is-full">
+        2. Single Interaction
+      </span>
+
+      <div class="columns is-centered">
+        <div class="column is-full">
+          <img
+            class="image"
+            src="../../../assets/img/ColliderBall/single_interaction.gif"
+          />
+        </div>
+      </div>
+      <div class="column is-full"><hr /></div>
+
+      <span class="image__caption ibm__typeface column is-full">
+        3. Multiple Interactions
+      </span>
+
+      <div class="columns is-centered">
+        <div class="column is-full">
+          <img
+            class="image"
+            src="../../../assets/img/ColliderBall/continuous_interaction.gif"
+          />
         </div>
       </div>
     </div>
