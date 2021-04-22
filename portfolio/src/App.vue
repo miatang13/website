a1c7fe53a1b9416854abe96f51a56beb018ae477
miatang <miatang13@gmail.com>
2021-04-22 11:02:51 -0700

    change word

--- portfolio/src/App.vue
+++ portfolio/src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
    <div class= "center">
    <img src = "https://64.media.tumblr.com/4e5d950cf3415279986f6b3df76df147/tumblr_mmxhygg0vn1rfjowdo1_500.gif" />
-   <h1 id="greeting"> Hi, I'm Mia Tang ( ˘ ³˘) </h1>
+   <h1 id="greeting"> Hey yo, I'm Mia Tang ( ˘ ³˘). </h1>
     <p> 
     I'm a junior at Carnegie Mellon <a href="https://www.cmu.edu/interdisciplinary/programs/bcsa.html">(BCSA)</a> 
     studying Computer Science and Communications Design w/ a minor in Mathem. Logic. <br><br>
@@ -14,59 +14,9 @@
       href="https://www.cmu.edu/news/stories/archives/2020/june/tool-turns-math-into-pictures.html"
       >Penrose</a
     > (Researcher), and soon to be at <a href = "https://www.jam3.com/"> Jam3</a> (Developer Intern). 
-    
-    Previous work available upon request. <br><br>
-    Find me on <a href="https://www.linkedin.com/in/mia-tang/"> LinkedIn</a>, or miatang13[at]gmail[dot]com. </p>
-    <!--
-    Previously worked as a Frontend Engineer at
-    <a href="https://www.linkedin.com/company/irisrover/mycompany/">
-      IrisRover</a
-    >
-    building interfaces for a lunar rover
-    
-    & a Researcher at <a
-      href="https://www.cmu.edu/news/stories/archives/2020/june/tool-turns-math-into-pictures.html"
-      >Penrose</a
-    >
-    creating programmable visualization tool for mathematical diagrams. <br> <br>
-    I'm very excited to be joining <a href = "https://www.jam3.com/"> Jam3</a>  in the summer 
-    as a developer intern. (Yay!)
-    <br><br>
-
-    In my free time, I enjoy writing articles and doing illustrations on computer science 
-    topics. 
-    </p>
-    <div id ="article-list">
-     <ul class="lists small__list desktop__only">
-              <li>
-                <a
-                  href="https://mia-tang.medium.com/functional-programming-manual-v-1-510ae8671b88"
-                >
-                  Functional Programming Manual V.1
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://mia-tang.medium.com/a-note-on-higher-order-functions-191dc1a48ed4"
-                >
-                  A Note On Higher Order Functions
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://mia-tang.medium.com/interactive-proof-ed5050e8e78d"
-                  >Interactive & Zero-Knowledge Proof</a
-                >
-              </li>
-            </ul>
-    </div>
-
-    <div id="contacts">
-      <p> Find me on <a href="https://www.linkedin.com/in/mia-tang/"> LinkedIn</a>, or miatang13[at]gmail[dot]com. </p>
-    </div>
-
+    <br> <br>
+    Portfolio is currently archived, but here are my dogs!  </p>
   </div>
-  -->
 
       <div class = "center doggos">
           <img
@@ -168,7 +118,7 @@ footer {
 }
 
 .doggos{ 
-  padding-top: 5vw;
+  padding-top: 2vw;
   padding-bottom: 5vh;
   max-width: 60%;
 }
@@ -176,15 +126,16 @@ footer {
 
 @media screen and (max-width: 600px) {
   h1 { 
-  font-size: 4vh;
+    font-size: 4vh;
   }
   p, li, #small-text{
     font-size: 2vh;
   }
   #static-text-container, .doggos, footer { 
     max-width: 100%;
-    padding: 3vh;
+    padding: 1vh;
   }
+
   ul { 
       list-style-type: none;
   }
