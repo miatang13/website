e39f3fb1e926f87548aefe3f66cc4eaf87e5a9eb
miatang13 <miatang13@gmail.com>
2022-01-23 19:46:28 -0800

    Use twitter embed widget

--- portfolio/src/pages/projects/Illustrations.js
+++ portfolio/src/pages/projects/Illustrations.js
@@ -8,6 +8,7 @@ import ZoomImageGallery from "../../components/Project/ZoomImageGallery";
 import RefLink from "../../components/Utility/RefLink";
 import ZoomablePicture from "../../components/Utility/ZoomablePicture";
 import project_data from "../../settings/projects.json";
+import { Tweet } from "react-twitter-widgets";
 
 export default function Illustrations() {
   const project_idx = project_data.length - 1;
@@ -55,77 +56,24 @@ export default function Illustrations() {
           ]}
           md={4}
         />
-        <blockquote class="twitter-tweet">
-          <p lang="en" dir="ltr">
-            Been reading a bit into the history of computer graphics
-            (1960-2000s). Converted some of the notes I took into these. What
-            are the milestones in graphics you remember from any decade?{" "}
-            <a href="https://t.co/J7oVmQlMEG">pic.twitter.com/J7oVmQlMEG</a>
-          </p>
-          &mdash; miatang (@Miamiamia0103){" "}
-          <a href="https://twitter.com/Miamiamia0103/status/1485314067610812420?ref_src=twsrc%5Etfw">
-            January 23, 2022
-          </a>
-        </blockquote>{" "}
+        <Tweet tweetId="1485314067610812420" />
       </ProjectSection>
 
       <ProjectSection subtitle="Occlusion with Z-buffer">
         <ZoomablePicture
           src={base_url + "graphics/comics/occlusion_zbuffer.png"}
         />
-        <blockquote class="twitter-tweet">
-          <p lang="en" dir="ltr">
-            Almost every beautiful view we look at both in physical world and
-            digital scenes involves having objects at different depth. Today we
-            look at how Z-buffer is a great technique to render objects at
-            different distances from the camera, which smartly handles
-            occlusion.{" "}
-            <a href="https://t.co/LHLucZxfS5">pic.twitter.com/LHLucZxfS5</a>
-          </p>
-          &mdash; miatang (@Miamiamia0103){" "}
-          <a href="https://twitter.com/Miamiamia0103/status/1454168094151225356?ref_src=twsrc%5Etfw">
-            October 29, 2021
-          </a>
-        </blockquote>{" "}
+        <Tweet tweetId="1454168094151225356" />
       </ProjectSection>
 
       <ProjectSection subtitle="Texture Mapping & MIP Map">
         <ZoomablePicture src={base_url + "graphics/comics/mipmap.png"} />
-        <blockquote class="twitter-tweet">
-          <p lang="en" dir="ltr">
-            Mipmap textures are used frequently in graphics to reduce aliasing
-            in images. Today we take a look at what is mipmap, why we need
-            mipmap, and how we generate mipmap.{" "}
-            <a href="https://t.co/6JvFgTjBgK">pic.twitter.com/6JvFgTjBgK</a>
-          </p>
-          &mdash; miatang (@Miamiamia0103){" "}
-          <a href="https://twitter.com/Miamiamia0103/status/1446842946813370369?ref_src=twsrc%5Etfw">
-            October 9, 2021
-          </a>
-        </blockquote>{" "}
+        <Tweet tweetId="1446842946813370369" />
       </ProjectSection>
 
       <ProjectSection subtitle="Anti-aliasing">
         <ZoomablePicture src={base_url + "graphics/comics/anti-aliasing.png"} />
-        <blockquote class="twitter-tweet">
-          <p lang="en" dir="ltr">
-            For the intro Computer Graphics class at{" "}
-            <a href="https://twitter.com/CarnegieMellon?ref_src=twsrc%5Etfw">
-              @CarnegieMellon
-            </a>
-            , we asked our students for feedback on how to improve the
-            documentation for an assignment on triangle rasterization &amp;
-            supersampling.
-            <br />
-            <br />
-            One of them created this (!):{" "}
-            <a href="https://t.co/NoRNbS1LJQ">pic.twitter.com/NoRNbS1LJQ</a>
-          </p>
-          &mdash; Keenan Crane (@keenanisalive){" "}
-          <a href="https://twitter.com/keenanisalive/status/1443930631554183168?ref_src=twsrc%5Etfw">
-            October 1, 2021
-          </a>
-        </blockquote>
+        <Tweet tweetId="1443930631554183168" />
       </ProjectSection>
 
       <ProjectSection subtitle="Interactive & Zero-Knowledge Proof">
