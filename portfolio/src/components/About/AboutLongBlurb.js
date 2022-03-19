1a325b364db5998c466042a73f8191bd9956ac5f
miatang13 <miatang13@gmail.com>
2022-03-19 19:59:13 -0400

    Update AboutLongBlurb.js

--- portfolio/src/components/About/AboutLongBlurb.js
+++ portfolio/src/components/About/AboutLongBlurb.js
@@ -43,7 +43,7 @@ export default function AboutLongBlurb() {
         </Col>
       </Row>
       <h3>
-        In the past, I met and collaborated with wonderful people as a developer
+        In the past, I have met and collaborated with wonderful people as a developer
         intern at <RefLink text="Jam3" href="https://www.jam3.com/" />, research
         assistant at{" "}
         <RefLink text="Penrose" href="https://penrose.cs.cmu.edu/ " />,
