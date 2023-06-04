31c3b4f77d15a8826ab52dfe312852212ce4536c
miatang13 <miatang13@gmail.com>
2023-06-04 00:20:24 -0700

    Update global-styles.js

--- settings/global-styles.js
+++ settings/global-styles.js
@@ -4,7 +4,7 @@ const globalStyles = {
     h2: "font-mono font-bold",
     h3: "font-mono",
     h4: "font-mono",
-    p: "font-mono font-medium",
+    p: "font-mono font-normal",
     nav_bar: "font-mono font-black hover:cursor-pointer hover:bg-pink-200",
     btn: "font-mono font-black hover:cursor-pointer hover:bg-pink-200 px-2",
     highlight_text: "font-black underline decoration-pink-500",
