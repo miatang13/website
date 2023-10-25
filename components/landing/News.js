45509967f8fcb433689bd63a99094b514fd05f12
miatang13 <miatang13@gmail.com>
2023-10-24 21:52:06 -0700

    Fix landing news

--- components/landing/News.js
+++ components/landing/News.js
@@ -1,105 +1,59 @@
 const dates_style = "text-slate-400 pr-2";
-const descriptions_style = "mono font-normal text-slate-800 p-2";
 import news from "../../settings/news";
-
-const months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
+import NewsRow from "./NewsRow";
 
 export default function News() {
   var news_by_year = {};
   news.forEach((item) => {
-    let month = item.month;
+    let month = parseInt(item.month);
+    let year = parseInt(item.year);
+    if (item.hide) {
+      return;
+    }
     if (item.year in news_by_year) {
-      if (month in news_by_year[item.year]) {
-        news_by_year[item.year][month].push(item);
+      if (month in news_by_year[year]) {
+        news_by_year[year][month].push(item);
       } else {
-        news_by_year[item.year][month] = [item];
+        news_by_year[year][month] = [item];
       }
     } else {
-      news_by_year[item.year] = {};
-      news_by_year[item.year][month] = [item];
+      news_by_year[year] = {};
+      news_by_year[year][month] = [item];
     }
   });
+  console.log("news_by_year", news_by_year);
 
-  // we want it to be reverse chronological order
-  var news_by_year_keys = Object.keys(news_by_year)
-    .sort()
-    .reverse();
+  // now we want to put all events in order for each year
+  let all_events = {};
+  let years = Object.keys(news_by_year);
+  for (let i = 0; i < years.length; i++) {
+    let year = years[i];
+    let ordered_events = [];
+    let months = Object.keys(news_by_year[year]);
+    let intMonths = months.map((item) => parseInt(item));
+    intMonths.sort((a, b) => a - b);
+    intMonths = intMonths.reverse();
+    console.log(intMonths);
+    for (let i = 0; i < intMonths.length; i++) {
+      ordered_events.push(news_by_year[year][intMonths[i]]);
+    }
+    console.log(ordered_events);
+    all_events[year] = ordered_events;
+  }
+  console.log("all_events", all_events);
 
-  console.log(news_by_year_keys);
   return (
     <ul className='list-none list-inside'>
-      {news_by_year_keys.map((year) => {
+      {Object.keys(all_events).map((year) => {
         return (
           <li className='grid md:grid-cols-6 pb-6' key={year}>
             <h4 className={dates_style}>{year}</h4>
             <div className='col-span-5 flex flex-col'>
-              {Object.keys(news_by_year[year])
-                .sort()
-                .reverse()
-                .map((month) => {
-                  return news_by_year[year][month].map((item) => {
-                    return (
-                      !item.hide && (
-                        <div
-                          className='grid md:grid-cols-5 my-1'
-                          key={item.description}
-                        >
-                          <div className='p-2'>
-                            <span className='pr-2'> {item.emoji}</span>
-                            <span
-                              className={
-                                item.future ? "text-blue-400" : "text-pink-400"
-                              }
-                            >
-                              {months[month - 1]}
-                            </span>
-                          </div>
-                          <div
-                            className={
-                              "rounded-sm col-span-4 " +
-                              (item.future ? "bg-blue-50" : "bg-pink-50") +
-                              (item.link
-                                ? " hover:bg-pink-200 hover:text-pink-800 cursor-pointer \
-                                transition duration-300 ease-in-out"
-                                : "")
-                            }
-                            key={item.description}
-                          >
-                            {item.link ? (
-                              <a
-                                href={item.link}
-                                target='_blank'
-                                rel='noopener noreferrer'
-                                className='underline'
-                              >
-                                <p className={descriptions_style}>
-                                  {item.description}
-                                </p>
-                              </a>
-                            ) : (
-                              <p className={descriptions_style}>
-                                {item.description}
-                              </p>
-                            )}
-                          </div>
-                        </div>
-                      )
-                    );
-                  });
-                })}
+              {all_events[year].map((yearEvents) => {
+                return yearEvents.map((item) => {
+                  return !item.hide && <NewsRow {...item} key={item.title} />;
+                });
+              })}
             </div>
           </li>
         );
