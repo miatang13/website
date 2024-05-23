0ee1ff136ea7ce06571ea3dfddad5b3c866ff074
miatang13 <miatang13@gmail.com>
2024-05-23 13:39:10 -0700

    Update Intro.js

--- components/landing/Intro.js
+++ components/landing/Intro.js
@@ -4,34 +4,27 @@ export default function LandingIntro() {
   return (
     <div className='pb-3 max-h-min'>
       <h4 className='mono font-normal text-slate-800'>
-        I am Mia — an MS CS student at Stanford University.
-        I am passionate about developing innovative
-        creative tools for artists and designers.
-        <br />
-        <br />
-        During my time at Carnegie Mellon University, where I received
-        Bachelors of Computer Science and Arts, I had the privilege of
-        conducting research on generative systems under the guidance of
-        Prof.
-        {` `}
-        <StyledExternalLink href='https://www.cs.cmu.edu/~junyanz/'>
+        I am Mia — a first-year master’s student in computer science at Stanford University, advised by Professor  {" "}
+        <StyledExternalLink href="https://graphics.stanford.edu/~maneesh/"> Maneesh Agrawala</StyledExternalLink>.
+        Previously, I earned my BCSA (Bachelor of Computer Science and Arts) at Carnegie Mellon University,
+        where I was fortunate to be advised by Professor <StyledExternalLink href='https://www.cs.cmu.edu/~junyanz/'>
           Jun-Yan Zhu
-        </StyledExternalLink>
-        , and computational design under the guidance of Prof. {` `}
-        <StyledExternalLink href='https://kyuhashim.com/'>
+        </StyledExternalLink> on generative visual systems and Professor <StyledExternalLink href='https://kyuhashim.com/'>
           Kyuha Shim
-        </StyledExternalLink>
-        .
-        <br />
-        <br />
-        Teaching is a passion of mine. I experiment as a comic artist, curating accessible educational
-        illustrations on technical topics. I have had the honor to co-teach courses at SIGGRAPH and
-        SIGGRAPH Asia on Generative AI, incorporating visual narratives in the teaching material. In my free time, I also
-        volunteer as a math tutor for preschool children at local schools in Palo Alto.
-        <br />
-        <br />
-        If you'd like to chat about design, generative systems, or education,
-        feel free to reach me at miatang13 [at] gmail.
+        </StyledExternalLink> on computational design.<br /><br />
+
+        My research interests are at the intersection of computer graphics, computational 2D and 3D design, and storytelling.
+        I aim to create compelling and engaging visual content by introducing new forms of control into vision-language models.<br /><br />
+
+        My primary goal in creating effective visuals is educational.
+        I am passionate about teaching and express this through my work as a comic artist,
+        developing accessible educational illustrations on technical topics.
+        I have had the privilege of co-teaching courses on Generative AI at SIGGRAPH and SIGGRAPH Asia.
+        Additionally, I volunteer as a math tutor for preschool children at local schools in Palo Alto in my free time.
+        <br /><br />
+
+        If you'd like to discuss generative models, design, or education, feel free to contact me at miatang13 [at] gmail.com.
+
       </h4>
     </div>
   );
