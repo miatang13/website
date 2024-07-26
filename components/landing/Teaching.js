ecc93c48e3cb1aeee483b49217f139f7a00b42bd
miatang13 <miatang13@gmail.com>
2024-07-26 15:39:32 -0700

    Update Teaching.js

--- components/landing/Teaching.js
+++ components/landing/Teaching.js
@@ -2,11 +2,27 @@ import StyledExternalLink from "../global/StyledExternalLink";
 
 const teachingData = [
     {
+        "name": "Generative Models for Visual Content Editing and Creation",
+        "venue": "ACM SIGGRAPH",
+        "link": "https://s2024.conference-program.org/presentation/?id=gensub_279&sess=sess166",
+        "semesters": ["July 2024"],
+        "role": "Instructor",
+        "future": true
+    },
+    {
+        "name": "Introduction to Generative Machine Learning",
+        "venue": "ACM SIGGRAPH",
+        "link": "https://s2024.conference-program.org/presentation/?id=gensub_449&sess=sess153",
+        "semesters": ["July 2024"],
+        "role": "Instructor",
+        "future": true
+    },
+    {
         "name": "The Future of Generative Visual Art",
         "venue": "CVPR",
         "link": "https://cveu.github.io/",
         "semesters": ["June 2024"],
-        "role": "CVEU Workshop Organizer"
+        "role": "Workshop Organizer"
     },
     {
         "name": "Computer Graphics: Rendering, Geometry, and Image Manipulation",
@@ -54,6 +70,7 @@ const teachingData = [
 
 const descriptions_style = "mono font-bold";
 const div_style = "bg-gradient-to-r from-pink-50 to-white-500 p-4 outline outline-offset-2 outline-pink-50 "
+const future_div_style = "bg-gradient-to-r from-blue-100 to-white-500 p-4 outline outline-offset-2 outline-blue-50 "
 
 export default function Teaching() {
     return (
@@ -62,16 +79,29 @@ export default function Teaching() {
                 <div key={index} className="flex justify-between items-center py-2 grid md:grid-cols-3 gap-y-2">
                     <p className="text-slate-400"> {details.venue}</p>
 
-                    <div className={div_style + "p-2 md:col-span-2 rounded-sm hover:bg-pink-200 hover:text-pink-500 cursor-pointer transition duration-100 ease-in-out"}>
-                        <a
-                            href={details.link}
-                            target='_blank'
-                            rel='noopener noreferrer'
-                        >
-                            <p className={descriptions_style}>{details.name}</p>
-                        </a>
-                        <p className="text-slate-400 text-sm">{details.role} for {details.semesters.join(', ')}</p>
-                    </div>
+                    {
+                        details.future ?
+                            <div className={future_div_style + "p-2 md:col-span-2 rounded-sm hover:bg-blue-200 hover:text-blue-500 cursor-pointer transition duration-100 ease-in-out"}>
+                                <a
+                                    href={details.link}
+                                    target='_blank'
+                                    rel='noopener noreferrer'
+                                >
+                                    <p className={descriptions_style}>{details.name}</p>
+                                </a>
+                                <p className="text-slate-400 text-sm">{details.role} for {details.semesters.join(', ')}</p> </div> :
+                            <div className={div_style + "p-2 md:col-span-2 rounded-sm hover:bg-pink-200 hover:text-pink-500 cursor-pointer transition duration-100 ease-in-out"}>
+                                <a
+                                    href={details.link}
+                                    target='_blank'
+                                    rel='noopener noreferrer'
+                                >
+                                    <p className={descriptions_style}>{details.name}</p>
+                                </a>
+                                <p className="text-slate-400 text-sm">{details.role} for {details.semesters.join(', ')}</p>
+                            </div>
+                    }
+
                 </div>
             ))}
         </div>
