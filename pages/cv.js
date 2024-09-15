7b822c6ef2c15fb18bfecc836c7f487752060092
miatang13 <miatang13@gmail.com>
2024-09-14 17:34:32 -0700

    Check if gdrive link works on mobile

--- pages/cv.js
+++ pages/cv.js
@@ -1,11 +1,17 @@
 const CvPage = () => {
     const resumePath = "/assets/personal/MiaTang_CV.pdf";
+    const fileID = "1vxVktg2mDVQ-foRNr4U731Jt8EcPQeSJ"
+    // const fileUrl = "https://drive.google.com/uc?export=view&id=" + fileID;
+    //const fileUrl = "https://drive.google.com/uc?export=view&id=1vxVktg2mDVQ-foRNr4U731Jt8EcPQeSJ"
+    const fileUrl = "https://drive.google.com/uc?export=view&id=1vxVktg2mDVQ-foRNr4U731Jt8EcPQeSJ";
+
     return (
         <div className="mt-[60px] h-[calc(100vh-60px)] flex justify-center items-center">
             <iframe
-                src={resumePath} // Make sure the path is correct and accessible
-                className="w-full h-full max-w-6xl"
+                src="https://drive.google.com/file/d/1vxVktg2mDVQ-foRNr4U731Jt8EcPQeSJ/preview" // Direct Google Drive embed link
+                className="w-full h-full max-w-4xl"
                 title="Resume PDF"
+                allow="autoplay"
             />
         </div>
     );
