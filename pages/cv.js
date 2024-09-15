ee7f9912c4fae029bac402bce550401ab286c306
miatang13 <miatang13@gmail.com>
2024-09-14 17:47:16 -0700

    Update cv.js

--- pages/cv.js
+++ pages/cv.js
@@ -1,15 +1,11 @@
 const CvPage = () => {
-    const resumePath = "/assets/personal/MiaTang_CV.pdf";
-    const fileID = "1vxVktg2mDVQ-foRNr4U731Jt8EcPQeSJ"
-    // const fileUrl = "https://drive.google.com/uc?export=view&id=" + fileID;
-    //const fileUrl = "https://drive.google.com/uc?export=view&id=1vxVktg2mDVQ-foRNr4U731Jt8EcPQeSJ"
-    const fileUrl = "https://drive.google.com/uc?export=view&id=1vxVktg2mDVQ-foRNr4U731Jt8EcPQeSJ";
+    const fileUrl = "https://drive.google.com/file/d/1vxVktg2mDVQ-foRNr4U731Jt8EcPQeSJ/preview"
 
     return (
         <div className="mt-[60px] h-[calc(100vh-60px)] flex justify-center items-center">
             <iframe
-                src="https://drive.google.com/file/d/1vxVktg2mDVQ-foRNr4U731Jt8EcPQeSJ/preview" // Direct Google Drive embed link
-                className="w-full h-full max-w-4xl"
+                src={fileUrl} Direct Google Drive embed link
+                className="w-full h-full max-w-4xl md:w-[80%] md:h-[85vh] md:rounded-lg md:border md:border-gray-200"
                 title="Resume PDF"
                 allow="autoplay"
             />
