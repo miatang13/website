d7f837c900bad3bc9ca90bb85584b8483564d93c
miatang13 <miatang13@gmail.com>
2024-09-19 16:30:16 -0700

    Update Intro.js

--- components/landing/Intro.js
+++ components/landing/Intro.js
@@ -9,7 +9,7 @@ export default function LandingIntro() {
       <div class="grid lg:grid-cols-6">
 
         {/* Left Image */}
-        <div className="lg:col-span-2 lg:mx-[10px] lg:px-20 lg:pt-3 w-full">
+        <div className="lg:col-span-2 lg:mx-[10px] lg:pr-28 lg:pl-0 lg:pb-0 lg:pt-3 p-14 w-full">
           <Image
             src={landingPic}
             className="w-full h-auto object-cover rounded-full"
@@ -17,9 +17,11 @@ export default function LandingIntro() {
           />
         </div>
         {/* Right Text Content */}
-        <div className="md:col-span-4 grid place-content-center md:pl-2 flex items-stretch">
+        <div className="md:col-span-4 grid place-content-center flex items-stretch">
           <div className="flex flex-col justify-between">
-            <h2 className={section_title_style}>Hello there :)</h2> <h4 className='mono font-normal text-slate-800'>
+            <h2 className={section_title_style + " mb-2"}>Hello there :)</h2>
+
+            <h4 className='mono font-normal text-slate-800 mt-0 pt-0'>
               I am Mia — a master's student in computer science at Stanford University, where I'm fortunate to be advised by Professor {" "}
               <StyledExternalLink href="https://graphics.stanford.edu/~maneesh/"> Maneesh Agrawala</StyledExternalLink>. I have also had the privilege of working at the <StyledExternalLink href="https://svl.stanford.edu/"> Stanford Vision and Learning Lab (SVL)</StyledExternalLink>.
               Previously, I earned my BCSA (Bachelor of Computer Science and Arts) at Carnegie Mellon University,
@@ -31,20 +33,30 @@ export default function LandingIntro() {
 
               I am passionate about teaching and express this through my work as a comic artist,
               developing accessible educational illustrations on technical topics. In my free time, I volunteer as a math tutor for preschool children at local schools in Palo Alto.
-
-              <br /><br />
-              <span class="text-pink-400 lg:text-sm">  🐰 I currently co-organize Stanford Graphics Cafe Seminar. Subscribe to our  <StyledExternalLink href="https://mailman.stanford.edu/mailman/listinfo/graphics-cafe">mailing list</StyledExternalLink> for updates! <br />
-                📧 You can reach me personally at miatang [at] cs [dot] stanford [dot] edu.  </span>
             </h4>
           </div>
         </div>
       </div>
-      <div id="research-details">
-        <h2 className={section_title_style}>Research Interest</h2>
-        <h4 className='mono font-normal text-slate-800'>
-          My research focuses on vision-language models for controllable 2D and 3D generation. I am particularly interested in developing algorithms and systems that facilitate the creative processes of designers and artists.<br /><br />
-        </h4>
+      <div id="research-details" className="grid lg:grid-cols-3">
+        <div class='flex flex-col col-start-2 col-span-2 '>
+          <h2 className={section_title_style + " mb-2"}> Research Interest</h2>
+          <h4 className='mono font-normal text-slate-800 mt-0 pt-0'>
+            My research focuses on vision-language models for controllable 2D and 3D generation. <br />I am particularly interested in developing algorithms and systems that facilitate the creative processes of designers and artists.<br /><br />
+          </h4>
+        </div>
+
       </div>
+
+      <div className="grid lg:grid-cols-3 mb-4">
+        <div id="intro-call-outs" class='flex flex-col gap-y-2 col-start-2 col-span-2'>
+          <p class="text-pink-400 lg:text-sm mono font-bold">  🐰 I currently co-organize Stanford Graphics Cafe Seminar. Subscribe to our  <StyledExternalLink href="https://mailman.stanford.edu/mailman/listinfo/graphics-cafe">mailing list</StyledExternalLink> for updates!
+          </p>
+          <p class="lg:text-sm  mono font-bold text-blue-400"> 📧 You can reach me personally at miatang [at] cs [dot] stanford [dot] edu. </p>
+        </div>
+
+
+      </div>
+
     </div>
   );
 }
