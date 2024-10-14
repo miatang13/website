ad6ec6bb0b261914a04726f3b0d2719abe97d7e8
miatang13 <miatang13@gmail.com>
2024-10-14 11:18:00 -0400

    Update StyledExternalLink.js

--- components/global/StyledExternalLink.js
+++ components/global/StyledExternalLink.js
@@ -3,7 +3,7 @@ export default function StyledExternalLink(props) {
     <a
       href={props.href}
       target='_blank'
-      className='underline hover:cursor-pointer hover:bg-pink-100 hover:text-pink-500'
+      className='underline hover:cursor-pointer hover:bg-pink-100 hover:text-pink-500 font-bold'
     >
       {props.children}
     </a>
