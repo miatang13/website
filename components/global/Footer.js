c1edf7baca93b5143bb315b948ebf70677551155
miatang13 <miatang13@gmail.com>
2024-10-16 03:05:54 -0400

    Add google scholar

--- components/global/Footer.js
+++ components/global/Footer.js
@@ -1,5 +1,6 @@
 import Linkedin from "../svgs/Linkedin";
 import Twitter from "../svgs/Twitter";
+import GoogleScholar from "../svgs/GoogleScholar";
 import globalStyles from "../../settings/global-styles";
 
 export default function Footer() {
@@ -7,7 +8,7 @@ export default function Footer() {
     <footer className='container sm:pt-8 sm:pb-8 pb-12 pt-4 w-screen place-content-center grid'>
       <ul className='flex flex-none sm:flex-1 space-x-4 place-content-center'>
         <li>
-          <a href='https://twitter.com/Miamiamia0103'>
+          <a href='https://twitter.com/Miamiamia0103' target="_blank">
             <Twitter />
           </a>
         </li>
@@ -15,8 +16,16 @@ export default function Footer() {
           <a
             className='align-baseline'
             href='https://www.linkedin.com/in/mia-tang/'
-          >
+            target="_blank">
             <Linkedin />
+          </a>
+        </li>
+        <li>
+          <a
+            className='align-baseline'
+            href='https://scholar.google.com/citations?user=4uGB3NsAAAAJ'
+            target="_blank">
+            <GoogleScholar />
           </a>
         </li>
       </ul>
