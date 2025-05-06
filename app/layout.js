3b7ccafbe1687ddc2710ffe78193ca4a3eea1325
miatang13 <miatang13@gmail.com>
2025-05-05 22:02:36 -0700

    Add analytics

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import { Analytics } from '@vercel/analytics/next';
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import Footer from "@/components/Footer";
@@ -28,7 +28,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-
+        <Analytics />
         <Footer />
       </body>
 
