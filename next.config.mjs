eb5939d3e86829b95e602a86906c5d54a74462be
miatang13 <miatang13@gmail.com>
2025-05-21 13:47:09 -0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,21 @@ const nextConfig = {
                 destination: '/',
                 permanent: true,
             },
+            {
+                source: "/work/comic-booklet",
+                destination: "/comic-booklet",
+                permanent: true,
+            },
+            {
+                source: "/work/conference-teaching-highlights",
+                destination: "/conference-teaching-highlights",
+                permanent: true,
+            },
+            {
+                source: "/work/g-cafe-bunnies",
+                destination: "/g-cafe-bunnies",
+                permanent: true,
+            }
         ];
     },
 };
