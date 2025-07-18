4530d822806e3916b81f54f1d016edd77358f674
miatang13 <miatang13@gmail.com>
2025-07-18 00:39:13 -0700

    Update Bio.js

--- components/Bio.js
+++ components/Bio.js
@@ -15,9 +15,9 @@ export default function Bio() {
     });
 
     const fadeInPropsNoY = (delay = 0) => ({
-        initial: { opacity: 0, },
-        whileInView: { opacity: 1, },
-        transition: { duration: 0.8, delay, ease: 'easeOut' },
+        initial: { opacity: 0, y: 20 },
+        whileInView: { opacity: 1, y: 0 },
+        transition: { duration: 0.5, delay, ease: 'easeOut' },
         viewport: { once: true, amount: 0.3 },
     });
 
