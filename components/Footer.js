7c45fe72b61d1f0a6a370a8107f90fd6e797c1f9
Mia Tang <miatang13@gmail.com>
2026-07-07 23:06:49 -0700

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@
 export default function Footer() {
     return (
         <footer className="pt-4 pb-8 text-center text-xs text-gray-500 mt-4 border-t border-gray-200">
-            <p>© mia tang / 2018-2025</p>
+            <p>© mia tang / 2018-2026</p>
             <p className="mt-1 italic">Have a wonderful day. ◡̈</p>
         </footer>
     );
